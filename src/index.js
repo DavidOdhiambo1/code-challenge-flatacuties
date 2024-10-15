940bb8308bcfb1a05680f29d5a29b445e6b750f6
DavidOdhiambo1 <davidworkmail2021@gmail.com>
2024-10-15 08:38:02 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,28 @@
 // Your code here
+//show characters names here
+
+const baseUrl = "http://localhost:3000"
+
+function showCharacterNames() {
+    fetch ( `${baseUrl}/characters`)
+    .then((response) => response.json())
+    .then(data => {characterDetails = data})
+
+}
+showCharacterNames()
+
+let characterDetails
+const characterDiv = document.getElementById("character-bar")
+
+fetch ( `${baseUrl}/characters`, {
+    method : "GET"
+})
+.then((response) => response.json())
+.then(data => {characterDetails = data})
+
+
+.then(()=>console.log(characterDetails[0]))
+
+
+
+
