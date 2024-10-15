da3f2667d511ded78e34c58100d05109bd0d9ba5
DavidOdhiambo1 <davidworkmail2021@gmail.com>
2024-10-15 09:53:51 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -11,11 +11,17 @@ function showCharacterNames() {
       const span = document.createElement('span')
       span.textContent = item.name
       characterDiv.appendChild(span)
+//display character image
+      span.addEventListener("click", ()=>{
+        const image = document.querySelector('img')
+        image.src = item.image
+        console.log(image.src)
+      })
     }))
 }
 showCharacterNames()
 
-//commit
+
 
 
 
